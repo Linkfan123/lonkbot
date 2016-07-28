cfabecd3feaf4acfb612808a73c52c441ce62553
Linkfan123 <linkfan321@outlook.com>
2016-07-28 15:52:54 -0500

    Code bruh

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ var Discord = require("discord.js");
 
 var mybot = new Discord.Client();
 
+
+    
 mybot.on("message", function(message) {
-    if(message.content === "!ping") {
+    mybot.setPlayingGame("ya mum")
+    if(message.content === "+-ping") {
         mybot.reply(message, "pong");
     }
-	 if(message.content === "!gnip") {
+	if(message.content === "+-gnip") {
         mybot.reply(message, "gnop");
+    }
+    if(message.content === "+-help") {
+        mybot.reply(message, "Ok buddy, I'll help you out. Check your PM's for a list of commands and what they do.")
+        mybot.sendmessage(message.id, "DEM COMMANDS:\nRemember, +- prefixes my commands, just in case you have any other supperior bots on your server ;)\n+-help -- Well, you know what that does.\n+-ping -- Makes me say pong to you.\n+-gnip -- Makes me say gnop to you.\nAnd dat's it.")
     }
 });
 
